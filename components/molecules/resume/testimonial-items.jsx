c3e578ffe12e68cedb29f80f32df71aa46884ee7
Auliya Raka Pratama <tama@sampingan.co.id>
2020-07-22 02:05:04 +0700

    feat(resume): add max height on role

--- components/molecules/resume/testimonial-items.jsx
+++ components/molecules/resume/testimonial-items.jsx
@@ -4,7 +4,11 @@ const TestimonialItem = (props) => {
 	return (
 		<div className="dib mt5 pa4 my-bg-dark-gray ba my-moon-white-border w-48">
 			<div className="moon-gray f3">{props.name}</div>
-			<div className="hot-pink f5 fw6">{props.role}</div>
+			<div
+				className="hot-pink f5 fw6"
+				style={{ height: '22px', overflowY: 'auto' }}>
+				{props.role}
+			</div>
 			<div
 				className="mt4 moon-gray f4"
 				style={{ height: '100px', overflowY: 'auto' }}>
