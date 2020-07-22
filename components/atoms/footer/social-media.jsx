bd1b3a84ad98c556dc678ba34ff9c918e3a5ce89
Auliya Raka Pratama <tama@sampingan.co.id>
2020-07-22 23:00:51 +0700

    fix(footer): fix link

--- components/atoms/footer/social-media.jsx
+++ components/atoms/footer/social-media.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const SocialMedia = (props) => {
 	return (
-		<a className="db white mt3 f3 f2-l hover-dark-pink" href={props.href}>
+		<a
+			className="db white mt3 f3 f2-l hover-dark-pink"
+			href={props.href}
+			target="_blank">
 			{props.children}
 		</a>
 	);
