c0c42ef7e8bd4ea1f0cf6343667915ab52385c7f
Kiki Pratiwi <kikipratiwi@gmail.com>
2023-04-04 11:34:25 +0700

    fix(contact): fix hydration by remove text inside text

--- components/organisms/ContactForm.tsx
+++ components/organisms/ContactForm.tsx
@@ -15,74 +15,76 @@ const OrganismContactForm = (): ReactElement => {
     };
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <VStack spacing={5} alignItems="start">
-                <HStack spacing={16}>
-                    <VStack alignItems="start">
-                        <Input
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <VStack spacing={5} alignItems="start">
+                    <HStack spacing={16}>
+                        <VStack alignItems="start">
+                            <Input
+                                focusBorderColor="#FF008C"
+                                placeholder="Full name"
+                                variant="flushed"
+                                width="290px"
+                                {...register('full_name', {
+                                    required: 'Please enter your name',
+                                })}
+                            />
+
+                            {errors?.full_name && (
+                                <Text fontSize="sm" color="pink.400">
+                                    {errors?.full_name?.message as any}
+                                </Text>
+                            )}
+                        </VStack>
+
+                        <VStack alignItems="start">
+                            <Input
+                                focusBorderColor="#FF008C"
+                                placeholder="Email"
+                                variant="flushed"
+                                width="290px"
+                                {...register('email', {
+                                    required: 'Please enter your email',
+                                    pattern: {
+                                        value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                                        message: 'Invalid email address',
+                                    },
+                                })}
+                            />
+
+                            {errors?.email && (
+                                <Text fontSize="sm" color="pink.400">
+                                    {errors?.email?.message as any}
+                                </Text>
+                            )}
+                        </VStack>
+                    </HStack>
+
+                    <VStack width="100%" alignItems="start">
+                        <Textarea
                             focusBorderColor="#FF008C"
-                            placeholder="Full name"
+                            placeholder="Tell me what you need"
+                            resize="none"
+                            rows={3}
                             variant="flushed"
-                            width="290px"
-                            {...register('full_name', {
-                                required: 'Please enter your name',
+                            {...register('description', {
+                                required: 'Please tell me your need',
                             })}
                         />
 
-                        {!!errors?.full_name && (
+                        {errors?.description && (
                             <Text fontSize="sm" color="pink.400">
-                                {errors?.full_name?.message as any}
+                                {errors?.description?.message as any}
                             </Text>
                         )}
                     </VStack>
 
-                    <VStack alignItems="start">
-                        <Input
-                            focusBorderColor="#FF008C"
-                            placeholder="Email"
-                            variant="flushed"
-                            width="290px"
-                            {...register('email', {
-                                required: 'Please enter your email',
-                                pattern: {
-                                    value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-                                    message: 'Invalid email address',
-                                },
-                            })}
-                        />
+                    <div style={{ marginTop: 12 }} />
 
-                        {!!errors?.email && (
-                            <Text fontSize="sm" color="pink.400">
-                                {errors?.email?.message as any}
-                            </Text>
-                        )}
-                    </VStack>
-                </HStack>
-
-                <VStack width="100%" alignItems="start">
-                    <Textarea
-                        focusBorderColor="#FF008C"
-                        placeholder="Tell me what you need"
-                        resize="none"
-                        rows={3}
-                        variant="flushed"
-                        {...register('description', {
-                            required: 'Please tell me your need',
-                        })}
-                    />
-
-                    {!!errors?.description && (
-                        <Text fontSize="sm" color="pink.400">
-                            {errors?.description?.message as any}
-                        </Text>
-                    )}
+                    <AtomButton type="submit">SEND A MESSAGE</AtomButton>
                 </VStack>
-
-                <div style={{ marginTop: 12 }} />
-
-                <AtomButton type="submit">SEND A MESSAGE</AtomButton>
-            </VStack>
-        </form>
+            </form>
+        </>
     );
 };
 
